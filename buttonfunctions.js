2b402cd9dbc6b68948a1e2e14070b58514e6a95c
rayoung0903 <32072466+rayoung0903@users.noreply.github.com>
2018-09-20 18:56:53 -0400

    Add files via upload

--- buttonfunctions.js
+++ buttonfunctions.js
@@ -13,13 +13,10 @@ function login() {
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
 
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function (error) {
-
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-
         window.alert("Error : " + errorMessage);
-
     });
 }
 
@@ -32,6 +29,24 @@ function register() {
         var errorCode = error.code;
         var errorMessage = error.message;
         window.alert("Error : " + errorMessage);
+    });
+
+    var user = firebase.auth().currentUser;
+    var userId = user.uid;
+    // Random base temp between 15 and 20.
+    var baseTemp1 = 15 + Math.random() * 5;
+
+    var time_array1 = [];
+    var temp_array1 = [];
+
+    for (i = 0; i < 100; i++) {
+        time_array1.push(i);
+        temp_array1.push(base_temp1 + Math.random());
+    }
+
+    firebase.database().ref(userId).set({
+        temp_array: temp_array1,
+        time_array: time_array1
     });
 
 }
